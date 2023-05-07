5b1507adc243c4cc3c9ab5874bcf408b30da664a
somunelavalli <somumstr210@gmail.com>
2023-05-07 18:01:04 +0530

    add console

--- controller/authController.js
+++ controller/authController.js
@@ -1,10 +1,11 @@
-require("dotenv").config();
-const User = require("../models/User");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const createError = require("../utils/error");
+require('dotenv').config();
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const createError = require('../utils/error');
 
 const register = async (req, res, next) => {
+  console.log('I am coming hereeee');
   try {
     const salt = bcrypt.genSaltSync(10);
     const hash = bcrypt.hashSync(req.body.password, salt);
@@ -23,19 +24,19 @@ const register = async (req, res, next) => {
 const login = async (req, res, next) => {
   try {
     const user = await User.findOne({ userName: req.body.userName });
-    if (!user) return next(createError(404, "User not found"));
+    if (!user) return next(createError(404, 'User not found'));
     const isPasswordCorrect = await bcrypt.compareSync(
       req.body.password,
       user.password
     );
-    if (!isPasswordCorrect) return next(createError(400, "Wrong Credentials"));
+    if (!isPasswordCorrect) return next(createError(400, 'Wrong Credentials'));
     const token = jwt.sign(
       { id: user._id, isAdmin: user.isAdmin },
       process.env.JWT_SECRET
     );
     const { password, isAdmin, ...otherDetails } = user._doc;
     res
-      .cookie("access_token", token, { httpOnly: true })
+      .cookie('access_token', token, { httpOnly: true })
       .status(200)
       .json({ ...otherDetails });
   } catch (err) {
