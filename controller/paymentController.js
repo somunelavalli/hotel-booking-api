a3292f97e469b29c2329ccd5e3d92ba8999127a6
somunelavalli <47572851+somunelavalli@users.noreply.github.com>
2023-07-04 15:32:41 +0530

    added cypto module in verify

--- controller/paymentController.js
+++ controller/paymentController.js
@@ -1,4 +1,5 @@
 const Razorpay = require('razorpay');
+var crypto = require("crypto");
 const instance = new Razorpay({
   key_id: process.env.RAZORPAY_KEY_ID,
   key_secret: process.env.RAZORPAY_KEY_SECRET,
